77edc9048c58db9f88f474ad99b99a1a91623309
Damianlillad <damiananyadike@gmail.com>
2022-10-13 13:59:40 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/**
+ * main - prints the msg
+ * Return: 0 if successful
+ * /
+#include <stdio.h>
+
+int main(void)
+{
+	char msg[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(msg);
+
+	return (0);
+}
+ 
